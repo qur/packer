9f33c2ac9d15bdca9363d332809e69b3999dbee2
Julian Phillips <julian@quantumfyre.co.uk>
2013-07-02 22:32:15 +0100

    builder/kvm: Copy the message from the new headless mode.
    Since we are running KVM headless, it seems to make sense.

--- builder/kvm/step_run.go
+++ builder/kvm/step_run.go
@@ -27,6 +27,7 @@ func (s *stepRun) Run(state map[string]interface{}) multistep.StepAction {
 	driver := state["driver"].(Driver)
 	ui := state["ui"].(packer.Ui)
 	kvmPath := state["kvm_path"].(string)
+	vncPort := state["vnc_port"].(uint)
 
 	// Set the KVM path so that we know we started the machine
 	s.bootTime = time.Now()
@@ -39,6 +40,11 @@ func (s *stepRun) Run(state map[string]interface{}) multistep.StepAction {
 		ui.Error(err.Error())
 		return multistep.ActionHalt
 	}
+
+	ui.Message(fmt.Sprintf(
+		"The VM will be run headless, without a GUI. If you want to\n"+
+		"view the screen of the VM, connect via VNC without a password to\n"+
+		"127.0.0.1:%d", vncPort))
 
 	// Wait the wait amount
 	if int64(config.BootWait) > 0 {
