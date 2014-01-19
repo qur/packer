3d7b5b5a701efef44e432f01eb09e6552ae1a98e
Julian Phillips <julian@quantumfyre.co.uk>
2014-01-19 18:32:44 +0000

    Expand Artifact API to expose build state
    In order that something consuming an artifact can have access to extra
    builder specific data add the State method which allows the caller to
    ask for arbitary values by name.

--- builder/libvirt/artifact.go
+++ builder/libvirt/artifact.go
@@ -5,11 +5,12 @@ import (
 	"os"
 )
 
-// Artifact is the result of running the VirtualBox builder, namely a set
+// Artifact is the result of running the libvirt builder, namely a set
 // of files associated with the resulting machine.
 type Artifact struct {
-	dir string
-	f   []string
+	dir   string
+	f     []string
+	state map[string]interface{}
 }
 
 func (*Artifact) BuilderId() string {
@@ -26,6 +27,11 @@ func (*Artifact) Id() string {
 
 func (a *Artifact) String() string {
 	return fmt.Sprintf("VM files in directory: %s", a.dir)
+}
+
+func (a *Artifact) State(name string) interface{} {
+	value, _ := a.state[name]
+	return value
 }
 
 func (a *Artifact) Destroy() error {
